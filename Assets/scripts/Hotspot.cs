08952c5f5bd24edf369acaa5c3f5850b5f91442b
James Wilson <wilsjame@oregonstate.edu>
2018-06-18 14:52:48 -0700

    use cube task as base
    Copying entire Assets and Project Settings folder.

---
+++ Assets/scripts/Hotspot.cs
@@ -0,0 +1,40 @@
+﻿/* Attach to game objects intended to be trigger points (hotspots). 
+ * This script works in conjuction with HotSpotColorChanger.cs. */
+
+using UnityEngine;
+
+public class Hotspot : MonoBehaviour
+{
+    public delegate void HotspotEntered(GameObject someCube);
+    public static event HotspotEntered OnEntered;
+
+    public delegate void HotspotExited(GameObject someCube);
+    public static event HotspotExited OnExited;
+
+    private void OnTriggerEnter(Collider other)
+    {
+  
+        if (OnEntered != null)
+        {
+            OnEntered(other.gameObject);
+
+			/* Call test function */
+			GameObject.Find ("GameObject_SpawnHotSpots").GetComponent<SpawnHotspots> ().HotSpotTriggerInstantiate ();
+
+			/* Remove this hotspot when triggered */
+			Destroy (this.gameObject);
+        }
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+
+        if (OnExited != null)
+        {
+            OnExited(other.gameObject);
+        }
+
+    }
+
+}
---
+++ Assets/scripts/HotspotColorChanger.cs
@@ -0,0 +1,66 @@
+﻿/* Attach to game objects intended to be hand draggable (cubes). 
+ * This script works in conjuction with HotSpot.cs. */
+
+using HoloToolkit.Unity.InputModule;
+using UnityEngine;
+
+[RequireComponent(typeof(Renderer))]
+public class HotspotColorChanger : MonoBehaviour
+{
+
+    /* Pick object's "triggered" color */
+    public Color activeColor = Color.red; 
+    private Material cachedMaterial;
+
+    private void Awake()
+    {
+        
+        /* Set object's initial color */ 
+        cachedMaterial = GetComponent<Renderer>().material;
+        cachedMaterial.SetColor("_Color", Color.white);
+    }
+
+    /* Prevent memory leaks */
+    private void OnEnable()
+    {
+        Hotspot.OnEntered += OnHotspotEnter;
+        Hotspot.OnExited += OnHotspotExit;
+    }
+
+    /* Prevent memory leaks */
+    private void OnDisable()
+    {
+        Hotspot.OnEntered -= OnHotspotEnter;
+        Hotspot.OnExited -= OnHotspotExit;
+    }
+
+    private void OnHotspotEnter(GameObject cube)
+    {
+
+        /* Change this object's color upon hotspot collision */
+        cachedMaterial = cube.GetComponent<Renderer>().material;
+        cachedMaterial.SetColor("_Color", activeColor);
+		
+		/* Change this object's transparency upon hotspot collision */ 
+		    Color c = cube.GetComponent<Renderer>().material.color;
+			c.a = 0.5f; // 0 - 1, where 0 is most transparent
+			cube.GetComponent<Renderer>().material.color = c;
+
+		
+        /* Disable hand dragging */
+        cube.GetComponent<HandDraggable>().enabled = false;
+    }
+
+    private void OnHotspotExit(GameObject cube)
+    {
+        
+        /* Change this object's exited color */ 
+        //cachedMaterial.SetColor("_Color", Color.white);
+    }
+
+    private void OnDestroy()
+    {
+        DestroyImmediate(cachedMaterial);
+    }
+
+}
